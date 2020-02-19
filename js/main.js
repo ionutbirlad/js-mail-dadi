80b3d2b3e7d6e653730f859e0e8cf47b09af1fb5
Ionut Birlad <birladionut@yahoo.it>
2020-02-19 15:37:06 +0100

    correzioni

--- js/main.js
+++ js/main.js
@@ -3,28 +3,32 @@
 // Prima di tutto mi creo le variabili che mi occorrono
 // Poi chiedo all'utente di inserire il suo indirizzo E-EMAIL
 // Dopodichè con un ciclo faccio confrontare la suddetta con ogni elemento del mio ARRAY
-// Se sussiste una corrispondenza visualizzerò un determinato messsaggio
-// Altrimenti cambio messsaggio
+// Se sussiste una corrispondenza visualizzerò un determinato messsaggio, altrimenti cambio messaggio
 
 
 // Creazione variabili
-var databaseEmail = ["Mario", "Gianni", "Federico", "Filippo"];
+var databaseEmail = ["mario@gmail.com", "gianni@yahoo.it", "federico@libero.mail", "filippo@gmail.it"];
 // console.log(databaseEmail);
 
 // Richiesta utente
 var emailUtente = prompt("Inserisci la tua E-mail");
 // console.log(emailUtente);
 
-// Confronto dati
-for (var i = 0; i < databaseEmail.length; i++) {
-  if (databaseEmail[i] == emailUtente) {
-    var corrispondenza = databaseEmail[i];
+// Verifica della correttezza di quello che l'utente ha inserito
+if (isNaN(emailUtente)) {
+  // Confronto dati
+  for (var i = 0; i < databaseEmail.length; i++) {
+    if (databaseEmail[i] == emailUtente) {
+      var corrispondenza = databaseEmail[i];
+    }
   }
-}
 
-// Messaggi in base alla situazione
-if (corrispondenza != undefined) {
-  console.log(corrispondenza + " sei stato invitato!!");
+  // Messaggi in base alla situazione
+  if (corrispondenza != undefined) {
+    console.log("Bentornato!!");
+  } else {
+    console.log("Non ti sei ancora registrato. Devi avere un account per usufruire del nostro servizio");
+  }
 } else {
-  console.log(emailUtente + " non sei stato inviato.. :(");
+  console.log("Inserire un' E-mail valida");
 }
